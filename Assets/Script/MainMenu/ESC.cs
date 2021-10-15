bc37c4b3204a8e9f5cc7e3c6f5ae05991e252b95
Nonood Whatever <kamik.suwannaratt@gmail.com>
2021-10-15 16:52:26 +0700

    moreStuff
    this is based

---
+++ Assets/Script/MainMenu/ESC.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESC : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+            print("im out");
+        }
+    }
+}
---
+++ Assets/Script/MainMenu/Option.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option : MonoBehaviour
+{
+    void Update()
+    {
+      
+    }
+    public void SetFullscreen(bool isfullscreen)
+    {
+        Screen.fullScreen = isfullscreen;
+    }
+}
